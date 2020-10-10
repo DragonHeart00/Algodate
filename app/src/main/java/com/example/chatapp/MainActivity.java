b8513cf6a39ecfcf78b64d2a965ffc57e09a510e
kasassulaiman <s195462@student.dtu.dk>
2020-10-10 18:18:56 +0200

    work with male and female function in main activity class

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
@@ -15,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> al;
     private ArrayAdapter<String> arrayAdapter;
+    private FirebaseAuth firebaseAuth;
     private int i;
 
 
@@ -27,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUserSex();
         al = new ArrayList<>();
         al.add("php");
         al.add("c");
@@ -36,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
         al.add("c++");
         al.add("css");
         al.add("javascript");
+
+
+
 
         arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
         SwipeFlingAdapterView flingContainer = findViewById(R.id.frame);
@@ -65,11 +83,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
-                // Ask for more data here
-                al.add("XML ".concat(String.valueOf(i)));
+               /* al.add("XML ".concat(String.valueOf(i)));
                 arrayAdapter.notifyDataSetChanged();
                 Log.d("LIST", "notified");
                 i++;
+
+                */
             }
 
             @Override
@@ -89,4 +108,100 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    private String userSex;
+    private String oppositeUserSex;
+    public void checkUserSex(){
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference maleDB= FirebaseDatabase.getInstance().getReference().child("Users").child("Male");
+        maleDB.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if (snapshot.getKey().equals(user.getUid())){
+                    userSex="Male";
+                    oppositeUserSex="Female";
+                    getOppositeSexUsers();
+
+                }
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        DatabaseReference femaleDB= FirebaseDatabase.getInstance().getReference().child("Users").child("Female");
+        femaleDB.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if (snapshot.getKey().equals(user.getUid())){
+                    userSex="Female";
+                    oppositeUserSex="Male";
+                    getOppositeSexUsers();
+
+                }
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+
+
+
+
+    public void getOppositeSexUsers(){
+        DatabaseReference oppositeSexDB= FirebaseDatabase.getInstance().getReference().child("Users").child(oppositeUserSex);
+        oppositeSexDB.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if (snapshot.exists()){
+
+                  al.add("PHP");
+                  al.add(snapshot.child("name").getValue().toString());
+                  arrayAdapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    public void logoutUser(View view) {
+        firebaseAuth.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginRegistrationActivity.class);
+        startActivity(intent);
+        finish();
+        return;
+
+    }
 }
